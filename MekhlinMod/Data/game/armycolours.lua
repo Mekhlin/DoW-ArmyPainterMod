1195c73714a223733ee39fffdf8bbb6db90e5b67
Christian Tang <git@tcdev.dk>
2020-04-22 16:59:57 +0200

    MekhlinMod

---
+++ MekhlinMod/Data/game/armycolours.lua
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------------------------------------
+-- Army Painter Presets
+-- (c) 2004 Relic Entertainment Inc.
+
+-- Modified 2020 by Mekhlin (https://github.com/Mekhlin)
+
+
+-- these are preset "Citadel Colours" used for the Army Painter screen
+-- format: { r, g, b }
+
+colour_presets =
+{
+	{ name = "$17500000", colour  = { 255, 255, 255} }, -- Skull White (Custom)
+	{ name = "$17500001", colour  = { 0, 0, 0} }, -- Chaos Black (Custom)
+	{ name = "$73024", colour  = { 70, 70, 70} }, -- Boltgun Metal
+	{ name = "$73025", colour  = { 128, 130, 132} }, -- Codex Grey
+	{ name = "$73026", colour  = { 190, 190, 190} }, -- Fortress Grey
+	{ name = "$73018", colour  = { 82, 73, 63} }, -- Tin Bitz
+	{ name = "$73019", colour  = { 108, 93, 61} }, -- Graveyard Earth
+	{ name = "$73020", colour  = { 158, 134, 65} }, -- Bubonic Brown
+	{ name = "$17500002", colour  = { 160, 134, 97} }, -- Bone (Custom)
+	{ name = "$73027", colour  = { 235, 215, 182} }, -- Bleached Bone
+	{ name = "$17500003", colour  = { 154, 140, 123} }, -- Kommando Khaki (Custom)
+	{ name = "$73013", colour  = { 148, 44, 44} }, -- Red Gore
+	{ name = "$73012", colour  = { 100, 39, 29} }, -- Dark Flesh
+	{ name = "$17500004", colour  = { 110, 20, 0} }, -- Blood Ravens Red (Custom)
+	{ name = "$17500005", colour  = { 121, 55, 33} }, -- Terracotta (Custom)
+	{ name = "$73014", colour  = { 209, 26, 26} }, -- Blood Red
+	{ name = "$73006", colour  = { 0, 38, 80} }, -- Midnight Blue
+	{ name = "$73007", colour  = { 25, 70, 103} }, -- Regal Blue
+	{ name = "$73008", colour  = { 20, 78, 166} }, -- Ultramarines Blue
+	{ name = "$73009", colour  = { 32, 130, 144} }, -- Hawk Turquoise
+	{ name = "$73010", colour  = { 129, 211, 235} }, -- Ice Blue
+	{ name = "$73011", colour  = { 147, 184, 204} }, -- Space Wolves Grey
+	{ name = "$73000", colour  = { 28, 64, 32} }, -- Dark Angels Green
+	{ name = "$73001", colour  = { 65, 87, 57} }, -- Catachan Green
+	{ name = "$73002", colour  = { 33, 139, 58} }, -- Snot Green
+	{ name = "$73003", colour  = { 101, 179, 69} }, -- Scorpion Green
+	{ name = "$73004", colour  = { 125, 146, 83} }, -- Camo Green
+	{ name = "$73005", colour  = { 184, 195, 157} }, -- Rotting Flesh
+	{ name = "$73015", colour  = { 218, 146, 130} }, -- Dwarf Flesh
+	{ name = "$73016", colour  = { 164, 62, 139} }, -- Warlock Purple
+	{ name = "$73017", colour  = { 110, 13, 108} }, -- Liche Purple
+	{ name = "$73023", colour  = { 243, 120, 30} }, -- Fiery Orange
+	{ name = "$73022", colour  = { 255, 191, 0} }, -- Golden Yellow
+	{ name = "$73021", colour  = { 255, 240, 0} } -- Bad Moon Yellow
+};
